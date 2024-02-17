cccccb3d93c90570d28e3371faefbcbb15d50637
GetBackToCode <ashishsingh02304@gmail.com>
2024-02-17 17:04:45 +0530

    conversation_fixed

--- main/routing.py
+++ main/routing.py
@@ -5,5 +5,5 @@ websocket_urlpatterns=[
     path("ws/",consumers.IndexConsumer.as_asgi()),
     path("ws/dashboard/<str:username>/",consumers.DashboardConsumer.as_asgi()),
     path("ws/messenger/",consumers.MessengerConsumer.as_asgi()),
-    path("ws/messenger/<int:id>/",consumers.ConversationConsumer.as_asgi()),
+    path("ws/messenger/conversation/<int:id>/",consumers.ConversationConsumer.as_asgi()),
 ]
